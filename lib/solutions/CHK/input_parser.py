564c843905b626c05a98b538d5ceac6bc5ed3306
sbx_user1051 <sbx_user1051@169.254.52.181>
2024-05-26 22:34:16 +0000

    Sun May 26 22:34:16 UTC 2024

--- lib/solutions/CHK/input_parser.py
+++ lib/solutions/CHK/input_parser.py
@@ -1,4 +1,8 @@
 import re
 
-def parse_input(input_string):
-    
+def parse_input(path):
+    with open(path) as input_file:
+        print(input_file.read())
+
+
+parse_input("./challenges/CHK_R4.txt")
