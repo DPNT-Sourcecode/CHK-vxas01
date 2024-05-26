55f284118a48b67718d3db261e7a22fede8f21d3
sbx_user1051 <sbx_user1051@169.254.52.181>
2024-05-26 22:37:16 +0000

    Sun May 26 22:37:16 UTC 2024

--- lib/solutions/CHK/input_parser.py
+++ lib/solutions/CHK/input_parser.py
@@ -2,7 +2,11 @@ import re
 
 def parse_input(path):
     with open(path) as input_file:
-        print(input_file.read())
+        file_content = input_file.read()
+        data = re.findall(r'\|\s+([A-Z])\s+\|\s+(\d+)\s+\|\s+(.*)\s+\|\n', file_content)
 
+        print(data)
+        print(len(data))
 
 parse_input("./challenges/CHK_R4.txt")
+
