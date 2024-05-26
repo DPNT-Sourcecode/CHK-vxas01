c32e0ba15af35867c2281e7a7c45330cc0a88374
sbx_user1051 <sbx_user1051@169.254.52.181>
2024-05-26 22:46:16 +0000

    Sun May 26 22:46:16 UTC 2024

--- lib/solutions/CHK/input_parser.py
+++ lib/solutions/CHK/input_parser.py
@@ -15,12 +15,19 @@ def parse_input(path):
             price = int(prices[i].strip())
             price_data[sku] = price
 
-            promo_raw = promos_raw[i].strip()
+            promo_raw_items = promos_raw[i].strip().split(', ')
+
+            for promo in promo_raw_items:
+                if 'for' in promo:
+                    # Regular promotion
+                else:
+                    # Free item promotion
 
     return price_data, promo_data, free_item_data
 
 
 parse_input("./challenges/CHK_R4.txt")
+
 
 
 
