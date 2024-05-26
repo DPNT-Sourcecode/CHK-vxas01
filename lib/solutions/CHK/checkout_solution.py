039fcd8cc7b2dc965d0cd73801d7524f63a6ede9
sbx_user1051 <sbx_user1051@169.254.52.181>
2024-05-26 21:33:39 +0000

    Sun May 26 21:33:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,10 +12,21 @@ def checkout(skus: str) -> int:
         'D': 15,
     }
 
-    def get_item_subtotal(sku: str) -> int:
-        """Calculate subtotal for given item type (single SKU)"""
-        # Account for any promotions
-        
+    promo_data = {
+        'A': {
+            'count': 3,
+            'price': 130,
+        },
+        'B': {
+            'count': 2,
+            'price': 45,
+        },
+    }
+
+    def get_item_subtotal(sku: str, count: int) -> int:
+        """Calculate subtotal for given item type (single SKU) and count."""
+        # Account for any promotions by using mod
+
 
     # Check for illegal input (it's unclear for now if the string is just 'AAA' or e.g. comma-separated,
     # let's assume just SKUs themselves for now
@@ -24,7 +35,8 @@ def checkout(skus: str) -> int:
 
     counter = Counter(skus)
 
-    return sum([get_item_subtotal[sku] * counter[sku] for sku in skus])
+    return sum([get_item_subtotal(sku, counter[sku]) for sku in skus])
+
 
 
 
