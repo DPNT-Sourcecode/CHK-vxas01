da2335fe8f06fef6bcee0df8141e64acc5bd132c
sbx_user1051 <sbx_user1051@169.254.52.181>
2024-05-26 22:49:16 +0000

    Sun May 26 22:49:16 UTC 2024

--- lib/solutions/CHK/input_parser.py
+++ lib/solutions/CHK/input_parser.py
@@ -1,10 +1,12 @@
+from collections import defaultdict
+
 import re
 
 def parse_input(path):
     with open(path) as input_file:
         price_data = {}
-        promo_data = {}
-        free_item_data = {}
+        promo_data = defaultdict(dict)
+        free_item_data = defaultdict(dict)
 
         file_content = input_file.read()
         data = re.findall(r'\|\s+([A-Z])\s+\|\s+(\d+)\s+\|\s+(.*)\s+\|\n', file_content)
@@ -17,16 +19,21 @@ def parse_input(path):
 
             promo_raw_items = promos_raw[i].strip().split(', ')
 
-            for promo in promo_raw_items:
-                if 'for' in promo:
+            for promo_str in promo_raw_items:
+                if 'for' in promo_str:
                     # Regular promotion
+                    quantity, sku, price = re.match(r'(\d+)([A-Z]) for (\d+)', promo_str)
+                    promo_data[sku][int(quantity)] = int(price)
                 else:
                     # Free item promotion
+                    quantity, sku, price = re.match(r'(\d+)([A-Z]) for (\d+)', promo_str)
+                    promo_data[sku][int(quantity)] = int(price)
 
     return price_data, promo_data, free_item_data
 
 
 parse_input("./challenges/CHK_R4.txt")
+
 
 
 
