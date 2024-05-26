4052a9dd6cdc2246906171376d545494d2e71e3e
sbx_user1051 <sbx_user1051@169.254.52.181>
2024-05-26 22:40:16 +0000

    Sun May 26 22:40:16 UTC 2024

--- lib/solutions/CHK/input_parser.py
+++ lib/solutions/CHK/input_parser.py
@@ -4,9 +4,14 @@ def parse_input(path):
     with open(path) as input_file:
         file_content = input_file.read()
         data = re.findall(r'\|\s+([A-Z])\s+\|\s+(\d+)\s+\|\s+(.*)\s+\|\n', file_content)
+        skus, prices, promos_raw = zip(*data)
 
-        print(data)
-        print(len(data))
+        for sku in range(len(skus)):
+            sku = skus[sku]
+            price = int(prices[sku].strip())
+            print(sku, price)
+
 
 parse_input("./challenges/CHK_R4.txt")
+
 
